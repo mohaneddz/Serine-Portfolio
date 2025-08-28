4c201446763f044b851764d30ce02e9177ee0a32
Mohaned <akumaxd5@gmail.com>
2025-08-28 18:00:21 +0100

    chore: separate portfolio feature group

---
+++ src/app/logofolio/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Return from "@/components/Return";
+import LogofolioMain from "@/sections/LogofolioMain";
+import LogofolioProjects from "@/sections/LogofolioProjects";
+
+import { useState } from "react";
+
+export default function LogofolioLayout() {
+
+  const [id, setId] = useState(1);
+
+  function changeLogo(id: number) {
+    setId(id);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
+  return (
+    <main className="full center flex-col min-h-screen py-8 pl-16">
+      
+      <LogofolioMain id={id} />
+      <LogofolioProjects changeLogo={changeLogo} />
+
+      <Return />
+    </main>
+  );
+}
